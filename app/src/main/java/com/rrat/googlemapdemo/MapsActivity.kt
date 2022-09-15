78c7ae5240d5731eb768ab8178f067932cc2527e
amonserrat <alvaro.monserrat@gmail.com>
2022-09-15 18:11:48 -0400

    Santiago lt lng, maps works!

--- app/src/main/java/com/rrat/googlemapdemo/MapsActivity.kt
+++ app/src/main/java/com/rrat/googlemapdemo/MapsActivity.kt
@@ -41,8 +41,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        val sydney = LatLng(-33.44925052284258, -70.66758645726445)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Santiago"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
